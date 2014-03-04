a6dcf7cf1d62583ac9b81aa5b7660919938726ed
Mike <korolev_m@ukr.net>
2014-03-04 16:54:55 -0500

    php payments page

--- payment/index.php
+++ payment/index.php
@@ -2,9 +2,22 @@
 <html lang="ru-RU">
 <head>
     <link href="/css/fonts.css" rel="stylesheet" type="text/css">
+    <link href="/css/login.css" rel="stylesheet" type="text/css" media="screen">
     <link href="/css/style.css" rel="stylesheet" type="text/css" media="screen">
     <script src="/js/jquery-1.9.1.min.js"></script>
-    <script src="/js/interface.js"></script>
+    <script type="text/javascript">
+        $(document).ready(function(){
+            console.log('ready');
+            //additional-info-colapser
+            var colapser = $('.additional-info-colapser .switcher'),
+                colapsCont = $('.additional-info');
+                
+                colapser.on('click',function(){
+                    colapsCont.toggleClass('hidden');
+                    colapser.toggleClass('open');
+                });
+        });
+    </script>
     <script type="text/javascript">
         var _gaq = _gaq || [];
         _gaq.push(['_setAccount', 'UA-38982546-1']);
@@ -31,40 +44,33 @@
 <body class="inner">
 <div class="top"></div>
 <div class="wrap">
-    <!-- Yandex.Metrika counter -->
-    <script type="text/javascript">
-        (function (d, w, c) {
-            (w[c] = w[c] || []).push(function () {
-                try {
-                    w.yaCounter22607524 = new Ya.Metrika({id: 22607524,
-                        webvisor: true,
-                        clickmap: true,
-                        trackLinks: true,
-                        accurateTrackBounce: true});
-                } catch (e) {
-                }
-            });
+ <!-- Yandex.Metrika counter -->
+<script type="text/javascript">
+(function (d, w, c) {
+    (w[c] = w[c] || []).push(function() {
+        try {
+            w.yaCounter22607524 = new Ya.Metrika({id:22607524,
+                    webvisor:true,
+                    clickmap:true,
+                    trackLinks:true,
+                    accurateTrackBounce:true});
+        } catch(e) { }
+    });
 
-            var n = d.getElementsByTagName("script")[0],
-                    s = d.createElement("script"),
-                    f = function () {
-                        n.parentNode.insertBefore(s, n);
-                    };
-            s.type = "text/javascript";
-            s.async = true;
-            s.src = (d.location.protocol == "https:" ? "https:" : "http:") + "//mc.yandex.ru/metrika/watch.js";
+    var n = d.getElementsByTagName("script")[0],
+        s = d.createElement("script"),
+        f = function () { n.parentNode.insertBefore(s, n); };
+    s.type = "text/javascript";
+    s.async = true;
+    s.src = (d.location.protocol == "https:" ? "https:" : "http:") + "//mc.yandex.ru/metrika/watch.js";
 
-            if (w.opera == "[object Opera]") {
-                d.addEventListener("DOMContentLoaded", f, false);
-            } else {
-                f();
-            }
-        })(document, window, "yandex_metrika_callbacks");
-    </script>
-    <noscript>
-        <div><img src="//mc.yandex.ru/watch/22607524" style="position:absolute; left:-9999px;" alt=""/></div>
-    </noscript>
-    <!-- /Yandex.Metrika counter -->
+    if (w.opera == "[object Opera]") {
+        d.addEventListener("DOMContentLoaded", f, false);
+    } else { f(); }
+})(document, window, "yandex_metrika_callbacks");
+</script>
+<noscript><div><img src="//mc.yandex.ru/watch/22607524" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+<!-- /Yandex.Metrika counter -->
     <header>
         <div class="logo fl">
             <a href="/" title="Всё Мое"></a>
@@ -83,149 +89,17 @@
         </nav>
     </header>
     <div class="container bottom payment-page">
-        <div class="wizard-navigation">
-            <div>
-                <span>
-                    1. ПРЕДЛОЖЕНИЕ
-                </span>
-                <a href="#">2. ВЫБОР СПОСОБА</a>
-                <a href="#">3. ОПЛАТА</a>
-            </div>
-        </div>
-        <h1>
-            Мы помогаем людям создать<br>
-            положительные изменения с своими деньгами.<br>
-            Сейчас Ваша очередь.
-
-        </h1>
-
-        <div class="additional-info-colapser">
-            <div class="switcher"></div>
-        </div>
-        <div class="additional-info">
-            <h2>Создай прогресс своих денежных средств!</h2>
-
-            <div class="progress_chart">
-                <span style="position: absolute" class="italic">
-                    Как это сделать:
-                </span>
-                <img src="/img/payment/graphics.png">
-            </div>
-            <div class="payment_description">
-                <h2 class="center">Содержание</h2>
-
-                <p class="center italic">Сервис включает в себя:</p>
-
-                <div class="float_left_column">
-                    <h3>Приложение iPhone</h3>
-
-                    <div class="list_box">
-                        <uL>
-                            <li> Одна сводная форма, 6 главных величин.</li>
-                            <li> Фаворитные транзакции в два действия.</li>
-                            <li> Ввод данных.</li>
-                            <li> Отслеживает 8 типов денежных средств.</li>
-                            <li> OnLine & OffLine.</li>
-                            <li> Подробные графики.</li>
-                            <li> Статистика.</li>
-                        </uL>
-                        <a class="out_link green" href="#"></a>
-                    </div>
-                    <div class="italic">
-                        Будет полезно людям, которые не любят хранить чеки и предпочтут фиксировать расходы в любом
-                        месте.
-                    </div>
-
-                </div>
-                <div class="float_left_column">
-                    <h3>Web-приложение</h3>
-
-                    <div class="list_box">
-                        <uL>
-                            <li>Доступен Ваш балансовый лист.</li>
-                            <li> Возможность создать и увидеть все платежи.</li>
-                            <li> Отслеживает 8 типов денежных средств.</li>
-                            <li> И будьте готовы идти дальше!</li>
-                        </uL>
-                        <a class="out_link blue" href="#"></a>
-                    </div>
-                    <div class="italic">
-                        Для пользователей, которые предпочтут увидеть информацию более развернуто, чем может дать
-                        мобильный телефон.
-                    </div>
-
-                </div>
-                <div class="center clear" style="padding: 40px 0 ">
-                    <img src="/img/payment/iPhoneImac.png">
-                </div>
-                <div class="testimonials">
-                    <div class="testimonials_header center">
-                        <h2>Клиенты о нас ...</h2>
-
-                        <div class="testimonials_navigation">
-                            <div class="nav_prev"></div>
-                            <div class="nav_next"></div>
-                        </div>
-                    </div>
-                    <div class="testimonials_holder">
-                        <div class="testimonials_body">
-                            <div class="testimonials_user">
-                                от Superbabochka
-                            </div>
-                            <div class="testimonials_user_message">
-                                <p>
-                                    Спасибо! Двигайтесь в том же направлении!
-                                </p>
-                            </div>
-                        </div>
-                        <div class="testimonials_body">
-                            <div class="testimonials_user">
-                                от Jou_Jizzy
-                            </div>
-                            <div class="testimonials_user_message">
-                                <p>
-                                Все отлично и хорошо! Прям хватает по полной:) Единственное что хотелось бы - вместо
-                                стартового экрана с почтой и паролем, вводить цифровой пароль из 4 цифр, как на защите
-                                разблокировки.
-                                </p>
-                            </div>
-                        </div>
-                        <div class="testimonials_body">
-                            <div class="testimonials_user">
-                                от IVAN312330
-                            </div>
-                            <div class="testimonials_user_message">
-                                <p>
-                                К плюсам отнесу простоту, минималистичный дизайн. То что надо простому человеку. Нет кучи
-                                дурацких настоек. Но конечно, чувствуется некоторая недоработанность, не очень удобная
-                                навигация. Не хватает раздела “Помощь”.
-                                </p>
-                            </div>
-                        </div>
-                    </div>
-
-                    <div class="clear"></div>
-                </div>
-                <div class="app_cost">
-                    <h3 class="center">Стоимость</h3>
-
-                    <p class="center italic">Каждый месяц, первые 3 зарегистрированных пользователя получют бесплатно
-                        подписку на месяц.</p>
-
-                    <div class="app_cost_table">
-                        <img src="/img/payment/Form-Cost.png"/>
-                    </div>
-                    <p class="center italic">
-                        Следите за своими денежными средствами, анализируйте, планируйте, будьте благополучны!
-                    </p>
-
-                    <p class="center">
-                        <a href="#">Условия тарифных планов</a>
-                    </p>
-                </div>
-            </div>
-        </div>
-        <div class="empty"></div>
+        <?php 
+            $action = 'intro';
+            $page = $_GET['action'];
+            $cost = $_GET['summ'];
+            if(isset($page)){
+                $action = $page;
+            }
+            $content = file_get_contents('template/'.$action.'.html');
+            echo "<!--".$content."-->";
+            echo $content;
+        ?>
     </div>
     <footer>
         <div class="in">
