4b19f4e38d298091815ae5992358dd4e2eaff3db
Michael <mkorolev@sfdev.com>
2014-06-20 23:41:41 +0400

    fast fix fancybox error in interface js

--- js/interface.js
+++ js/interface.js
@@ -1,72 +1,74 @@
-$(document).ready(function(){
-	$('div.navi ul li').click(function(){
-	    var num = $(this).attr('data-num');
-	    $('div.navi ul li').removeClass('selected');
-	    $('.elements .phone').addClass('hidden');
-	    $('.elements .phone[data-num=' + num + ']').removeClass('hidden');
-	    $(this).addClass('selected');
-	});
+$(document).ready(function () {
+    $('div.navi ul li').click(function () {
+        var num = $(this).attr('data-num');
+        $('div.navi ul li').removeClass('selected');
+        $('.elements .phone').addClass('hidden');
+        $('.elements .phone[data-num=' + num + ']').removeClass('hidden');
+        $(this).addClass('selected');
+    });
 
 
-	$("a#example").fancybox({
-	    'titleShow': false
-	});
-	$("a#i1").fancybox({
-	    'titleShow': false
-	});
-	$("a#i2").fancybox({
-	    'titleShow': false
-	});
-	$("a#i3").fancybox({
-	    'titleShow': false
-	});
-	$("a#i4").fancybox({
-	    'titleShow': false
-	});
+    if ($.fancybox) {
+        $("a#example").fancybox({
+            'titleShow': false
+        });
+        $("a#i1").fancybox({
+            'titleShow': false
+        });
+        $("a#i2").fancybox({
+            'titleShow': false
+        });
+        $("a#i3").fancybox({
+            'titleShow': false
+        });
+        $("a#i4").fancybox({
+            'titleShow': false
+        });
+    }
 
-	$('#Span1').click(function(){
-	    $('#Div1').toggle();
-	});
-	$('#Span2').click(function (){
-	    $('#Div2').toggle();
-	});
-	$('#Span3').click(function (){
-	    $('#Div3').toggle();
-	});
-	$('#Span4').click(function (){
-	    $('#Div4').toggle();
-	});
+    $('#Span1').click(function () {
+        $('#Div1').toggle();
+    });
+    $('#Span2').click(function () {
+        $('#Div2').toggle();
+    });
+    $('#Span3').click(function () {
+        $('#Div3').toggle();
+    });
+    $('#Span4').click(function () {
+        $('#Div4').toggle();
+    });
 
-	$('#Span5').click(function () {
-	    $('#Div1').toggle();
-	});
-	$('#Span6').click(function () {
-	    $('#Div2').toggle();
-	});
-	$('#Span7').click(function () {
-	    $('#Div3').toggle();
-	});
-	$('#Span8').click(function () {
-	    $('#Div4').toggle();
-	});
+    $('#Span5').click(function () {
+        $('#Div1').toggle();
+    });
+    $('#Span6').click(function () {
+        $('#Div2').toggle();
+    });
+    $('#Span7').click(function () {
+        $('#Div3').toggle();
+    });
+    $('#Span8').click(function () {
+        $('#Div4').toggle();
+    });
 
-	$('#Div5').hover(function () {
+    $('#Div5').hover(function () {
         $('#Span5').toggle();
         $('#A5').toggle();
     });
-	$('#Div6').hover(function () {
+    $('#Div6').hover(function () {
         $('#Span6').toggle();
         $('#A6').toggle();
     });
-	$('#Div7').hover(function () {
+    $('#Div7').hover(function () {
         $('#Span7').toggle();
         $('#A7').toggle();
     });
-	$('#Div8').hover(function () {
+    $('#Div8').hover(function () {
         $('#Span8').toggle();
         $('#A8').toggle();
-	});
-	slider();
+    });
+    slider();
 });
 
 
@@ -74,7 +76,9 @@ function slider() {
     var selectElement = $('.elements div:not(.hidden_slide)');
     var num = parseInt(selectElement.attr('data-num'));
     num = num + 1;
-    if (num > 4) { num = 1; }
+    if (num > 4) {
+        num = 1;
+    }
     $('.elements .phone').addClass('hidden_slide');
     $('.elements .phone[data-num=' + num + ']').removeClass('hidden_slide');
 
