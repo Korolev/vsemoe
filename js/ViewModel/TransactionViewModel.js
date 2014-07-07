1b5e53802679b9cda2246ed4fa56239e4be9541d
Michael <mkorolev@sfdev.com>
2014-07-07 11:57:56 +0400

    merge with SEO

--- js/ViewModel/TransactionViewModel.js
+++ js/ViewModel/TransactionViewModel.js
@@ -25,6 +25,13 @@ var TransactionViewModel = function (data, app) {
 
     this.action_index = 0;
 
+    if (app.accountsHash[self.from_id]
+        && app.accountsHash[self.from_id]
+        && self.from_id != self.to_id
+        && !app.accountsHash[self.to_id].createFormAcc()) {
+        app.accountsHash[self.to_id].createFormAcc(app.accountsHash[self.from_id]);
+    }
+
     if(self.from_id && self.to_id && (self.from_id != self.to_id)){
         (function(){
             var from_type,
