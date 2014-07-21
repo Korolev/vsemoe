07589c2d507fd1e1f55401da6791834017415244
Michael <mkorolev@sfdev.com>
2014-07-21 19:26:34 +0400

    add widget to account; remove from site

--- js/header.js
+++ js/header.js
@@ -8,27 +8,3 @@ $(function () {
         $('header').css({ 'opacity': '1' });
     });
 });
-
-var _ues = {
-    host: 'vsemoe.userecho.com',
-    forum: '31891',
-    lang: 'ru',
-    tab_corner_radius: 2,
-    tab_font_size: 20,
-    tab_image_hash: '0L7RgdGC0LDQstC40YLRjCDQvtGC0LfRi9Cy',
-    tab_chat_hash: '0YfQsNGC',
-    tab_alignment: 'left',
-    tab_text_color: '#FFFFFF',
-    tab_text_shadow_color: '#00000055',
-    tab_bg_color: '#B6C1C6',
-    tab_hover_color: '#76A2CA'
-};
-
-(function () {
-    var _ue = document.createElement('script');
-    _ue.type = 'text/javascript';
-    _ue.async = true;
-    _ue.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + 'cdn.userecho.com/js/widget-1.4.gz.js';
-    var s = document.getElementsByTagName('script')[0];
-    s.parentNode.insertBefore(_ue, s);
-})();
